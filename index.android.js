f198aa60c7a5e23eac248c3cef64d91aab5b084f
Andrey <filinandrew@mail.ru>
2015-12-30 18:34:51 +0300

    Set image and title

--- index.android.js
+++ index.android.js
@@ -12,21 +12,41 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
 } = React;
 
 
 var MovieViewer = React.createClass({
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch('http://clickdev.me/dl/films.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
+        <Image
+          source={{uri: 'https://upload.wikimedia.org/wikipedia/en/0/01/Whiplash_poster.jpg'}}
+          style={styles.thumbnail}
+        />
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+          Title
         </Text>
       </View>
     );
@@ -41,17 +61,17 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  }
 });
 
 AppRegistry.registerComponent('MovieViewer', () => MovieViewer);
